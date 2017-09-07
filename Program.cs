80ec8df077307810270dfce90ed05ac4d5009c0d
MAVERICK <sps_no@hotmail.com>
2017-09-07 11:26:34 -0500

    Inception

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Drawing;
+
+namespace ImageToBytes
+{
+	class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] imgArray;
+            Image srcImg = Image.FromFile(@"C:\PathImage");
+            Program execute = new ImageToBytes.Program();
+
+            imgArray = execute.ImageToByteArrayPNG(srcImg);
+
+            System.Console.WriteLine(Encoding.UTF8.GetString(imgArray));
+            System.Console.ReadLine();
+
+        }
+
+        public byte[] ImageToByteArrayPNG(Image imageIn)
+        {
+            using (var ms = new MemoryStream())
+            {
+                imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+    }
+}
